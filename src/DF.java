956bba0bf1b2a8864e97825f970d80f1945ce901
David Forero Santillana <deivi@192.168.1.130>
2024-11-25 20:34:23 +0100

    “Modificación 3”

--- src/DF.java
+++ src/DF.java
@@ -4,7 +4,7 @@ public class DF {
 	public static void main(String[] args) {
 
 		System.out.println("Alumno:_David_Forero");
-		System.out.println("Primera Modificación desde Master"); 
+		System.out.println("Modificación desde Master - Cambiado"); 
 	}
 
 }
